dc9bd50c4e7b6265cb25206ced10603fcc07e02d
Anurag Sidhu <anuragsidhu72@gmail.com>
2023-12-04 19:00:51 +0530

    done

--- middleware.ts
+++ middleware.ts
@@ -10,8 +10,8 @@ export default withAuth({
 
 export const config = {
     matcher: [
-        // "/home",
-        // "/sales",
-        // "/driver"
+        "/home",
+        "/sales",
+        "/driver"
     ]
 };
