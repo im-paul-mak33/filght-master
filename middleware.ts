f9ed4b860af8afe0de51b78aaed87b304c23d176
Anurag Sidhu <anuragsidhu72@gmail.com>
2023-12-04 18:03:18 +0530

    middleware --Production

--- middleware.ts
+++ middleware.ts
@@ -2,17 +2,16 @@ import { withAuth } from "next-auth/middleware";
 
 export default withAuth({
     pages: {
-        signIn: "http://localhost:3000",
+        // signIn: "http://localhost:3000",
         // signIn: "https://filght.vercel.app/",
-        // signIn: "https://bados.vercel.app/",
+        signIn: "https://bados.vercel.app/",
     },
-
 });
 
 export const config = {
     matcher: [
-        // "/home",
-        // "/sales",
-        // "/driver"
+        "/home",
+        "/sales",
+        "/driver"
     ]
 };
