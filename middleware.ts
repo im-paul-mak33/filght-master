126a1a94a27a265797d38dc9ab348fc342d059d9
Anurag Sidhu <anuragsidhu72@gmail.com>
2023-12-04 10:55:26 +0530

    middleware 2 --Production

--- middleware.ts
+++ middleware.ts
@@ -2,9 +2,10 @@ import { withAuth } from "next-auth/middleware";
 
 export default withAuth({
     pages: {
+         signIn: "/auth/signin",
         // signIn: "http://localhost:3000",
         // signIn: "https://filght.vercel.app/",
-        signIn: "https://bados.vercel.app/",
+        // signIn: "https://bados.vercel.app/",
     },
 
 });
